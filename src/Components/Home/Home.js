039f881708cd09d273e8cffef7e5207a061183dc
MD FORKAN <mforka9@gmail.com>
2022-08-28 17:57:12 +0600

    first commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavHeader from '../MainHomeItem/NavHeader/NavHeader';
+
+const Home = () => {
+    return (
+        <div>
+            <NavHeader></NavHeader>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/MainHomeItem/NavHeader/NavHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavHeader.scss'
+
+const NavHeader = () => {
+    return (
+        <div>
+            <h1>navHeader</h1>
+        </div>
+    );
+};
+
+export default NavHeader;
